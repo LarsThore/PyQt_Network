1cfdb0f74166d9b2df41a574ea49843eb0a16251
LarsThore <thore.bergmann@fau.de>
2017-01-11 16:57:03 +0100

    added print function

---
+++ Examples/01-SimpleApp.py
@@ -0,0 +1,60 @@
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+import sys
+import time
+
+app = QApplication(sys.argv)
+due = QTime.currentTime()
+message = "Alert!"
+
+
+try:
+    if len(sys.argv) < 2:
+        raise ValueError
+
+    hours, minutes = sys.argv[1].split(':')
+    due = QTime(int(hours), int(minutes))
+
+    if not due.isValid():
+        raise ValueError
+
+    if len(sys.argv) > 2:
+        message = " ".join(sys.argv[2:])
+
+
+except:
+    print ("python3 01-SimpleApp.py HH:MM Optional Message")
+    sys.exit(0)
+
+
+while QTime.currentTime() < due:
+    time.sleep(5)
+
+
+label = QLabel("<font color = red size = 72>" + message + "< /font >")
+label.show()
+
+QTimer.singleShot(10000, app.quit)
+app.exec_()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Examples/02-ExpressionEval.py
@@ -0,0 +1,75 @@
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+import sys
+import math
+
+
+class Form(QDialog):
+
+    def __init__(self, parent = None):
+        super(Form, self).__init__(parent)      # old form of initializing (and using super and init)
+        # super().__init__()
+
+
+        self.resultsList = QTextBrowser()
+        self.resultsInput = QLineEdit("Enter an expression and press return key.")
+
+
+        layout = QVBoxLayout()      # VBox = Vertical Box (the widows are aligned beneath each other)
+        layout.addWidget(self.resultsList)
+        layout.addWidget(self.resultsInput)     # add both of the widgets to the layout
+        self.setLayout(layout)      # set the layout to the real layout of the form (i.e. Form)
+
+        self.resultsInput.selectAll()
+        self.resultsInput.setFocus()        # makes sure that the focus is on resultsInput and the text is preselected
+
+        self.resultsInput.returnPressed.connect(self.compute)   # by pressing the return key a signal is created
+                                                                # which is passed to the compute method
+
+
+    def compute(self):
+        try:
+            text = self.resultsInput.text()
+            self.resultsList.append("{0} = <b> {1} </b>".format(text, eval(text)))
+        except:
+            print("<font color = red> <b> Invalid Expression! </b> </font>")
+
+
+
+
+
+
+
+app = QApplication(sys.argv)
+form = Form()
+form.show()
+app.exec_()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
