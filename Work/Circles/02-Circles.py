ecced8758c5c6f5ed41dc3dbef6f87460258c067
LarsThore <thore.bergmann@fau.de>
2017-03-16 15:31:20 +0100

    Changed file structure

---
+++ Work/Circles/02-Circles.py
@@ -0,0 +1,63 @@
+from PyQt5.QtCore import Qt, QRectF
+from PyQt5.QtGui import QColor, QPen, QBrush, QCursor
+from PyQt5.QtWidgets import (QWidget, QApplication, QGraphicsEllipseItem,
+ QGraphicsScene, QGraphicsView)
+
+import sys
+import numpy as np
+
+__appname__ = "Draw Circles"
+
+class drawCircles(QWidget):
+
+    def __init__(self, parent = None):
+        super().__init__()
+
+        scene = QGraphicsScene()
+        scene.addText("Hello, world!")
+
+        view = QGraphicsView(scene)
+        view.show()
+
+    # def initUI(self):
+        # self.setGeometry(400, 200, 1000, 700)
+        # self.setWindowTitle("Circles")
+
+        # view.show()
+
+    # def paintEvent(self, e):            # Wofür steht das e?
+    #
+    #     qp = QGraphicsEllipseItem()
+    #     position = QCursor.pos()
+    #
+    #     qp.setBrush(Qt.red)
+    #     qp.setPen(Qt.red)
+    #     size = self.size()
+    #
+    #     QGraphicsEllipseItem(size.width() / 2, size.height()/2 , 20.0, 20.0)
+
+
+    def draw_circles(self, qp):
+
+        qp.setBrush(Qt.red)
+        qp.setPen(Qt.red)
+        size = self.size()
+
+        for i in range(15):
+            x = np.random.randint(10, size.width()-10)
+            y = np.random.randint(10, size.height()-10)
+
+            qp.drawEllipse(x, y, 20.0, 20.0)
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    form = drawCircles()
+    form.show()
+    app.exec_()
---
+++ Work/Circles/NW05Circles.py
@@ -0,0 +1,272 @@
+#! -*- coding: utf-8 -*- #
+
+from PyQt5.QtCore import Qt, QRectF, QEvent
+from PyQt5.QtGui import QColor, QPen, QBrush, QImage, QPainter
+from PyQt5.QtWidgets import QWidget, QApplication, QFileDialog
+from PyQt5.uic import loadUi
+
+from types import MethodType
+import sys
+import numpy as np
+
+app = QApplication(sys.argv)
+__appname__ = "Red Network"
+
+##################### importing the file made qt designer #####################
+w = loadUi("04-circles.ui")
+
+#################### image for saving the picture of circles ##################
+img = QImage(w.widget.width(), w.widget.height(), QImage.Format_RGB32)
+img.fill(Qt.white)              # image appears white in the beginning (not black)
+
+################################ set painter ##################################
+imgPainter = QPainter()         # first painter
+painter = QPainter()            # second painter
+
+################################## set pen ####################################
+line_drawer = QPen()
+line_drawer.setWidth(4)
+
+############################ set switch and lists #############################
+switch = 0      # switch at 0 for first circle, at 1 for second circle
+edge_counter = 0
+
+total_edge_length = 0
+
+start_point_list = [0]
+end_point_list = [0]
+coordinate_set = set()
+
+midpoints = []
+edges = []
+
+############################### help functions ################################
+
+def export_nodes():
+    """
+    Uses the global list 'midpoints' to export the data points of the nodes,
+    so that they can be saved as a .txt file.
+    """
+    fileObj = QFileDialog.getSaveFileName(w)
+
+    print (fileObj)
+    print (type(fileObj))
+
+    header = "Node positions \n \nx-coordinates \ty-coordinates \n"
+    content = ""
+
+    for point in midpoints:
+        content += str(point.x()) + "\t" + str(point.y()) + "\n"
+
+    fileName = fileObj[0]
+
+    with open(fileName, "w") as file:
+        file.write(header + content)
+
+def export_edges():
+    """
+    Uses the global list 'edges' to export the data points of the edges,
+    so that they can be saved as a .txt file.
+    """
+    fileObj = QFileDialog.getSaveFileName(w)
+
+    header = "Edge positions \n \nStart Point (x, y) \tEnd Point (x, y)\n \n"
+    content = ""
+
+    for tupel in edges:
+        counter = 0
+        for edge in tupel:
+            if counter % 2 == 0:
+                content += str(edge.x()) + "\t" + str(edge.y()) + "\t"
+            else:
+                content += str(edge.x()) + "\t" + str(edge.y()) + "\n"
+            counter += 1
+
+    fileName = fileObj[0]
+
+    with open(fileName, "w") as file:
+        file.write(header + content)
+
+def draw_circles(x, y):
+    """
+    Takes the x and y data of the event (mouse click) and draws a circle with
+    the QPainter 'painter' around that point with the radius (r) 10.
+    """
+    print ( tuple((x, y)) ),
+
+    r = 10
+    imgPainter.begin(img)          # use first painter to draw on image
+    imgPainter.setBrush(Qt.red)
+    imgPainter.drawEllipse(x-r, y-r, 2*r, 2*r)    # draw circle (circles are represented as an ellipse)
+    imgPainter.end()
+
+def fill_coordinate_set(x, y):
+    """
+    Takes the x and y data of the event (mouse click) and fills the set
+    'coordinate_set' with all the data points that surround this midpoint
+    """
+    for x_distance in range(-10, 11):            # set that will contain all pixels inside the circle
+        for y_distance in range(-10, 11):        # (or still inside a square around the circle center)
+            coordinate_set.add(tuple((x + x_distance, y + y_distance)))
+
+def find_closest_midpoint(event):
+    """
+    Takes the event (mouse click) and puts the start or end point of an edge
+    directly onto the corresponding midpoint of a node. So all the edges start
+    and end exactly in the middle of each node.
+    """
+    diff_list = []
+
+    for position in midpoints:
+        diff0 = abs(position.x() - event.pos().x())
+        diff1 = abs(position.y() - event.pos().y())
+        diff_list.append(sum(tuple((diff0, diff1))))
+
+    diff_array = np.array(diff_list)
+    i = np.argmin(diff_array)
+
+    return  midpoints[i]
+
+def save_starting_point(event):
+    '''
+    saves the starting point of the line after a click on the widget
+    '''
+
+    start_point = find_closest_midpoint(event)
+    print ("start")
+    start_point_list[0] = start_point
+
+def calculate_edge_length(end_point_list):
+    """
+    Takes the current end point - which is saved as only member in the list
+    'end_point_list' - and the start point as the only member of the global
+    list 'start_point_list' and calculates the corresponding distance after
+    Pythagoras.
+    The distances are accumulated in the variable 'total_edge_length'.
+    """
+    global total_edge_length
+
+    start = start_point_list[0]
+    end = end_point_list[0]
+
+    x_diff = abs(start.x() - end.x())
+    y_diff = abs(start.y() - end.y())
+
+    distance = np.sqrt(x_diff**2 + y_diff**2)
+    total_edge_length += distance
+
+def draw_line(event):
+    '''
+    uses the starting point and the second point - which is indicated by
+    another click - to draw a line between those points
+    '''
+
+    # end_point = event.pos()
+    end_point = find_closest_midpoint(event)
+    print ("end")
+    if end_point == start_point_list[0]:
+        raise ValueError("Start point and end point are equal.")
+    end_point_list[0] = end_point
+
+    edges.append( tuple((start_point_list[0], end_point_list[0])) )
+
+    imgPainter.begin(img)          # use first painter to draw on image
+    imgPainter.setPen(line_drawer)
+    imgPainter.drawLine(start_point_list[0], end_point)    # draw line from first circle to second circle
+    imgPainter.end()
+
+    calculate_edge_length(end_point_list)
+
+############################### main function #################################
+
+def drawing(self, event):
+    print (event.type())
+    global switch, edge_counter
+
+    print (event.type() == QEvent.MouseButtonPress and
+            tuple((event.pos().x(), event.pos().y())) not in coordinate_set)
+
+    # True if mouse click happens with cursor on free surface
+    if (event.type() == QEvent.MouseButtonPress and
+        tuple((event.pos().x(), event.pos().y())) not in coordinate_set):
+
+        midpoints.append(event.pos())
+        circle_center = event.pos()
+        x = circle_center.x()
+        y = circle_center.y()
+
+        draw_circles(x, y)
+        fill_coordinate_set(x, y)       # set value to dictionary key 'circle center'
+        self.update()                   # requests a paint event
+
+        w.node_label.setText("Nodes:\n" + str(len(midpoints)))
+
+    # True if mouse click happens with cursor on a node (and switch == 0)
+    elif (event.type() == QEvent.MouseButtonPress and switch == 0 and
+            tuple((event.pos().x(), event.pos().y())) in coordinate_set):
+
+        save_starting_point(event)
+        switch = 1
+        print ("switch: ", switch)
+
+    # True if mouse click happens with cursor on a node (and switch == 1)
+    elif (event.type() == QEvent.MouseButtonPress and switch == 1 and
+            tuple((event.pos().x(), event.pos().y())) in coordinate_set):
+
+        try:
+            draw_line(event)
+            self.update()                           # requests a paint event
+            switch = 0
+            print ("switch: ", switch)
+
+            edge_counter += 1
+            w.edge_label.setText("Edges:\n" + str(edge_counter))
+            w.total_length_label.setText("Total edge length:\n" + str(int(total_edge_length)))
+
+        except ValueError:
+            pass
+
+    # True if 'self.update()' is called
+    elif event.type() == QEvent.Paint:          # (you're only allowed to draw here (in a paint event) ?)
+        painter.begin(self)                     # use second painter to draw image on widget
+        painter.drawImage(0, 0, img)
+        painter.end()
+
+    return True                                 # return 'True' so that the event handler
+                                                # knows that the event is completed
+
+
+def erase():
+    '''
+    Deletes all Nodes and Edges and resets all lists and counters to the default
+    values.
+    '''
+
+    global switch, edge_counter, total_edge_length, start_point_list
+    global end_point_list, coordinate_set, midpoints, edges
+
+    switch = 0
+    edge_counter = 0
+    total_edge_length = 0
+    start_point_list = [0]
+    end_point_list = [0]
+    coordinate_set = set()
+    midpoints = []
+    edges = []
+
+    img.fill(Qt.white)
+    w.widget.update()
+    w.node_label.setText("Nodes:\n" + str(len(midpoints)))
+    w.edge_label.setText("Edges:\n" + str(len(edges)))
+    w.total_length_label.setText("Total edge length:\n" + str(total_edge_length))
+
+
+if __name__ == '__main__':
+    w.widget.event = MethodType(drawing, w.widget)  # ersetzt die Funktion, die die Ereignisse behandelt
+
+    w.eraseButton.clicked.connect(erase)
+    w.Export_Nodes.clicked.connect(export_nodes)
+    w.Export_Edges.clicked.connect(export_edges)
+
+    w.show()
+    sys.exit(app.exec_())
